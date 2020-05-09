d40796dc8cb7256a9ed6f44bda6bdb52dd244fb7
Emma <brocklix@icloud.com>
2020-05-09 17:16:02 -0500

    emma's cool site

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,7 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           {/* PWA primary color */}
+          <meta name="Description" content="Emma's cool site.">
           <meta name="theme-color" />
         </Head>
         <body>
